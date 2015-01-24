c2d2ecaab160a7fcb8c3351734d4c0a42c32410f
smshin7 <scottshin1@gmail.com>
2015-01-23 19:01:53 -0800

    added fixed navbar

--- app/uploaders/profile_pic_uploader.rb
+++ app/uploaders/profile_pic_uploader.rb
@@ -17,12 +17,12 @@ class ProfilePicUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  
+    "/images/fallback/" + [version_name, "default.jpg"].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
@@ -32,11 +32,11 @@ class ProfilePicUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process :resize_to_fit => [100, 100]
-  end
+  # version :thumb do
+  #   process :resize_to_fit => [100, 100]
+  # end
   version :profile do
-    process :resize_to_fit => [200, 200]
+    process :resize_to_fit => [400, 400]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
