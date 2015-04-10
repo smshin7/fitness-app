42e91fe2f6a495c21d9123bc4119bd0b2c754420
Scott Shin <scottshin1@gmail.com>
2015-04-10 16:46:06 -0700

    Update strength.rb

--- app/models/strength.rb
+++ app/models/strength.rb
@@ -1,7 +1,7 @@
 class Strength
 	include Mongoid::Document
 	field :pounds, type: Integer
-  field :sets, type: Integer
+  	field :sets, type: Integer
  	field :reps, type: Integer
 
  	embedded_in :workout
